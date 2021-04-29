e5425ae2f182b9118b082df269da3be84866d23a
Dominic <drmusic2021@gmail.com>
2021-04-29 12:54:15 +0100

    updated code for my blog

--- js/scripts.js
+++ js/scripts.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
               }
             },
             {
-              breakpoint: 600,
+              breakpoint: 700,
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
